aa3d69ff883d4caa535586f5ce3b565b1bf14e4f
Armin Shalchian <armin536@gmail.com>
2023-07-12 10:03:46 +0330

    Added support for macOS build

--- Package.swift
+++ Package.swift
@@ -7,7 +7,8 @@ let package = Package(
     name: "LightChart",
     platforms: [
         .iOS(.v13),
-        .watchOS(.v6)
+        .watchOS(.v6),
+        .macOS(.v10_15)
     ],
     products: [
         .library(
